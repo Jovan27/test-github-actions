9920f04b45a4cd9fb001c2120e67501caca6130a
Jovan Balaban <jovanbalaban27@gmail.com>
2022-01-26 18:53:41 +0100

    make a failing test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const add = (a, b) => a + b;
+const add = (a, b) => a - b;
 const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
 
-module.exports = { add, sub };
+module.exports = { add, sub, div };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { add, sub } = require('./');
+const { add, sub, div } = require('./');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
@@ -6,4 +6,8 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('subtracts 5 - 1 to equal 4', () => {
   expect(sub(5, 1)).toBe(4);
+});
+
+test('divides 10 with 5 to equal 2', () => {
+  expect(div(10, 5)).toBe(2);
 });
